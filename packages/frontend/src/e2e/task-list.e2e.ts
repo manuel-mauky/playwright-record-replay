e3ec4f364e29ab94e72213a811cfa6535677eae6
manuel <github@lestard.eu>
2025-06-20 12:56:22 +0200

    add tests of error message

--- packages/frontend/src/e2e/task-list.e2e.ts
+++ packages/frontend/src/e2e/task-list.e2e.ts
@@ -1,10 +1,24 @@
 import { test } from "./utils/fixtures.ts"
 import { expect } from "@playwright/test"
 import { TasksPageObject } from "./tasks.page.ts"
+import { BASE_API_URL } from "../tasks-api.ts"
 
-test("has title", async ({ page }) => {
-  const tasksPage = new TasksPageObject(page)
-  await tasksPage.goto()
+test.describe("task-list", () => {
+  test("has title", async ({ page }) => {
+    const tasksPage = new TasksPageObject(page)
+    await tasksPage.goto()
 
-  await expect(page).toHaveTitle(/Task Example/)
+    await expect(page).toHaveTitle(/Task Example/)
+  })
+
+  test("show error message when API is not available", async ({ page }) => {
+    await page.route(`${BASE_API_URL}/**/*`, (route) => {
+      return route.abort("connectionrefused")
+    })
+
+    const tasksPage = new TasksPageObject(page)
+    await tasksPage.goto()
+
+    await expect(page.getByText("Error: Failed to fetch")).toBeVisible({ timeout: 15_000 })
+  })
 })
